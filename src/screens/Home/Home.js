4a4c7500d08583462e4b031a9a6550750c9c0034
Iryna Arkhypchenko <37odri@gmail.com>
2021-03-21 17:29:05 +0200

    [5]: Data Fetching

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   View,
   Text,
@@ -7,34 +7,7 @@ import {
   TouchableOpacity,
 } from 'react-native';
 import { SOLARIZED, RAINBOW, FRONTEND_MASTERS } from '../../constants/Colors';
-import ColorsPreview from '../../components/ColorsPreview';
 import PalettePreview from '../../components/PalettePreview';
-
-// My solution
-// const Home = ({ navigation }) => {
-//   const handlePress = (screen, options) => {
-//     navigation.navigate(screen, options);
-//   };
-//   return (
-//     <View style={styles.screenWrapper}>
-//       <ColorsPreview
-//         colors={SOLARIZED}
-//         paletteName="Solarized"
-//         onNavigate={handlePress}
-//       />
-//       <ColorsPreview
-//         colors={RAINBOW}
-//         paletteName="Rainbow"
-//         onNavigate={handlePress}
-//       />
-//       <ColorsPreview
-//         colors={FRONTEND_MASTERS}
-//         paletteName="Frontend Master"
-//         onNavigate={handlePress}
-//       />
-//     </View>
-//   );
-// };
 
 // Teacher solution
 
@@ -45,6 +18,23 @@ const COLOR_PALETTES = [
 ];
 
 const Home = ({ navigation }) => {
+  const [palettes, setPalettes] = useState([]);
+
+  const fetchPalettes = useCallback(async () => {
+    const response = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+
+    if (response.ok) {
+      const palettesRes = await response.json();
+      setPalettes(palettesRes);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchPalettes();
+  }, [fetchPalettes]);
+
   const handlePress = (item) => {
     navigation.navigate('ColorPalette', item);
   };
@@ -52,11 +42,12 @@ const Home = ({ navigation }) => {
     <View style={styles.screenWrapper}>
       <FlatList
         style={styles.list}
-        data={COLOR_PALETTES}
+        data={palettes}
         keyExtractor={(item) => item.paletteName}
         renderItem={({ item }) => (
           <PalettePreview onPress={() => handlePress(item)} palette={item} />
         )}
+        ListEmptyComponent={<Text>There is no palette available...</Text>}
       />
     </View>
   );
